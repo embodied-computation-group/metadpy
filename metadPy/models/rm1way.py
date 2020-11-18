ab1ffffa839a25c49d368c3d3ed431b6a37d0015
LegrandNico <nicolas.legrand@cfin.au.dk>
2020-11-18 15:33:31 +0100

    change Gamma priors and add tracked variable

--- metadPy/models/rm1way.py
+++ metadPy/models/rm1way.py
@@ -95,7 +95,7 @@ def hmetad_rm1way(data, chains=3, tune=1000, draws=1000, cores=None):
         Bd_Cond1 = Normal(
             "Bd_Cond1", mu=mu_Cond1, tau=lambda_Cond1, shape=(1, nSubj, 1)
         )
-        tau = Gamma("tau", alpha=0.01, beta=0.01, shape=(1, nSubj, 1))
+        tau = Gamma("tau", alpha=0.001, beta=0.001, shape=(1, nSubj, 1))
 
         ###############################
         # Hypterprior - Condition level
@@ -110,7 +110,7 @@ def hmetad_rm1way(data, chains=3, tune=1000, draws=1000, cores=None):
 
         mu_regression = Deterministic("mu_regression", dbase + Bd_Cond1 * cond)
         logMratio = Normal(
-            "logMratio", mu_regression, tau=tau, shape=(1, nSubj, nCond)
+            "logMratio", mu_regression, sigma=tau, shape=(1, nSubj, nCond)
         )
         mRatio = Deterministic("mRatio", math.exp(logMratio))
 
@@ -291,6 +291,7 @@ def hmetad_rm1way(data, chains=3, tune=1000, draws=1000, cores=None):
                 metad,
                 tau,
                 dbase,
+                Bd_Cond1,
             ],
         )
 
