90b6243f2067306f826b86d693e081c98e33f22f
LegrandNico <nicolas.legrand@cfin.au.dk>
2020-12-14 00:14:56 +0100

    Add dataset

---
+++ metadPy/datasets/__init__.py
@@ -0,0 +1,22 @@
+# Author: Nicolas Legrand <nicolas.legrand@cfin.au.dk>
+
+import os
+import pandas as pd
+
+
+def load_dataset(dataset):
+    """Load simulated dataset
+
+    Parameters
+    ----------
+    dataset : str
+        Which dataset. Defalut is `'rm'`
+
+    Returns
+    -------
+    df : :py:class:`pandas.DataFrame`
+        Pandas dataframe.
+    """
+    path = ('https://github.com/LegrandNico/metadPy/raw/'
+            'master/metadPy/datasets/')
+    return pd.read_csv(op.join(path, 'rm.txt'))
