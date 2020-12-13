a284447c07330770f41a7e58479fe7391bf92833
LegrandNico <nicolas.legrand@cfin.au.dk>
2020-12-14 00:17:54 +0100

    Add tests for hierarchical module

--- metadPy/tests/test_hierarchical.py
+++ metadPy/tests/test_hierarchical.py
@@ -7,21 +7,21 @@ import numpy as np
 import pytest
 
 from metadPy.hierarchical import extractParameters, hmetad
-from metadPy.utils import ratings2df
-
-nR_S1 = np.array([52, 32, 35, 37, 26, 12, 4, 2])
-nR_S2 = np.array([2, 5, 15, 22, 33, 38, 40, 45])
-
+from metadPy.utils import ratings2df, load_dataset
+import pymc3 as pm
 
 class Testsdt(TestCase):
     def test_preprocess(self):
         """Test preprocess function"""
+        nR_S1 = np.array([52, 32, 35, 37, 26, 12, 4, 2])
+        nR_S2 = np.array([2, 5, 15, 22, 33, 38, 40, 45])
         data = extractParameters(nR_S1, nR_S2)
         assert round(data["d1"], 3) == 1.535
         assert round(data["c1"]) == 0
         assert np.all(
             data["counts"]
-            == np.array([52, 32, 35, 37, 26, 12, 4, 2, 2, 5, 15, 22, 33, 38, 40, 45])
+            == np.array([52, 32, 35, 37, 26, 12, 4, 2, 2,
+                         5, 15, 22, 33, 38, 40, 45])
         )
         assert data["nratings"] == 4
         assert data["Tol"] == 1e-05
@@ -34,18 +34,27 @@ class Testsdt(TestCase):
 
     def test_hmetad(self):
         """Test hmetad function"""
-        data = ratings2df(nR_S1, nR_S2)
-        model, trace = hmetad(
-            data,
-            stimuli="Stimuli",
-            accuracy="Accuracy",
-            confidence="Confidence",
-            nRatings=4,
-            chains=1,
-            tune=100,
-            draws=100,
-        )
-        assert int(trace["metad"].mean()) == 1
+        group_df = load_dataset('rm')
+
+        # Test subject level
+        ####################
+        this_df = group_df[(group_df.Subject == 0) &
+                           (group_df.Condition == 0)]
+        model, trace = hmetad(data=this_df, nRatings=4, stimuli='Stimuli',
+                              accuracy='Accuracy', confidence='Confidence')
+        assert isinstance(model, pm.Model)
+        assert isinstance(trace, pm.backends.base.MultiTrace)
+        assert round(trace['metad'].mean()) == 1
+
+        # Test repeated measure
+        #######################
+        model, trace = hmetad(data=group_df, nRatings=4, stimuli='Stimuli',
+                              accuracy='Accuracy', confidence='Confidence',
+                              subject='Subject', within='Condition', cores=2)
+        assert isinstance(model, pm.Model)
+        assert isinstance(trace, pm.backends.base.MultiTrace)
+        assert round(trace['mRatio'].mean(0)[0][:, 0].mean() - 1) == 0
+        assert round(trace['mRatio'].mean(0)[0][:, 0].mean() - .6) == 0
 
 
 if __name__ == "__main__":
